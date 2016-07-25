62394b8c16ebfbad9bfb33b6b1a486e9428e4a90
joe shin <joe.shin@live.com>
2016-07-24 23:18:18 -0400

    wasn't able to get retrieve table data

--- customer.js
+++ customer.js
@@ -28,18 +28,18 @@ var runSearch = function() {
             case 'Cleaning':
                 cleaningSearch();
             break;
-           /* 
+            
             case 'Find all artists who appear more than once':
-                multiSearch();
+                entertainmentSearch();
             break;
             
             case 'Find data within a specific range':
-                rangeSearch();
+                clothingSearch();
             break;
             
             case 'Search for a specific song':
-                songSearch();
-            break;*/
+                foodSearch();
+            break;
         }
     })
 };
@@ -52,7 +52,7 @@ var cleaningSearch = function() {
     }).then(function(answer) {
         var query = 'SELECT id, ProductName, DepartmentName, Price, StockQuantity FROM Products WHERE ?'
         connection.query(query, {cleaning: answer.cleaning}, function(err, res) {
-            for (var i = 0; i < res.length; i++) {
+            for (var i = 0; i < answer.length; i++) {
                 console.log("ID:" + res[i].id + " || ProductName: " + res[i].ProductName + " || DepartmentName:" + res[i].DepartmentName + " || Price:" + res[i].Price + " || StockQuantity:" + res[i].StockQuanity);
             }
             runSearch();
@@ -60,44 +60,32 @@ var cleaningSearch = function() {
     })
 };
 
-/*var entertainmentSearch = function() {
-    var query = 'SELECT artist FROM top5000 GROUP BY artist HAVING count(*) > 1';
-    connection.query(query, function(err, res) {
-        for (var i = 0; i < res.length; i++) {
-            console.log(res[i].artist);
-        }
-        runSearch();
+var entertainmentSearch = function() {
+    inquirer.prompt({
+        name: "Entertainment",
+        type: "input",
+        message: "Which entertainment item would you like to purchase?"
+    }).then(function(answer) {
+        var query = 'SELECT id, ProductName, DepartmentName, Price, StockQuantity FROM Products WHERE ?'
+        connection.query(query, {entertainment: answer.entertainment}, function(err, res) {
+            for (var i = 0; i < answer.length; i++) {
+                console.log("ID:" + res[i].id + " || ProductName: " + res[i].ProductName + " || DepartmentName:" + res[i].DepartmentName + " || Price:" + res[i].Price + " || StockQuantity:" + res[i].StockQuanity);
+            }
+            runSearch();
+        })
     })
 };
 
 var clothingSearch = function() {
-    inquirer.prompt([{
-        name: "start",
+    inquirer.prompt({
+        name: "Clothing",
         type: "input",
-        message: "Enter starting position: ",
-        validate: function(value) {
-            if (isNaN(value) == false) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-    }, {
-        name: "end",
-        type: "input",
-        message: "Enter ending position: ",
-        validate: function(value) {
-            if (isNaN(value) == false) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-    }]).then(function(answer) {
-        var query = 'SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?';
-        connection.query(query, [answer.start, answer.end], function(err, res) {
-            for (var i = 0; i < res.length; i++) {
-                console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Artist: " + res[i].artist + " || Year: " + res[i].year);
+        message: "Which clothing item would you like to purchase?"
+    }).then(function(answer) {
+        var query = 'SELECT id, ProductName, DepartmentName, Price, StockQuantity FROM Products WHERE ?'
+        connection.query(query, {clothing: answer.clothing}, function(err, res) {
+            for (var i = 0; i < answer.length; i++) {
+                console.log("ID:" + res[i].id + " || ProductName: " + res[i].ProductName + " || DepartmentName:" + res[i].DepartmentName + " || Price:" + res[i].Price + " || StockQuantity:" + res[i].StockQuanity);
             }
             runSearch();
         })
@@ -106,17 +94,16 @@ var clothingSearch = function() {
 
 var foodSearch = function() {
     inquirer.prompt({
-        name: "song",
+        name: "Food",
         type: "input",
-        message: "What song would you like to look for?"
+        message: "Which food item would you like to purchase?"
     }).then(function(answer) {
-        console.log(answer.song)
-        connection.query('SELECT * FROM top5000 WHERE ?', {song: answer.song}, function(err, res) {
-            console.log("Position: " + res[0].position + " || Song: " + res[0].song + " || Artist: " + res[0].artist + " || Year: " + res[0].year);
+        var query = 'SELECT id, ProductName, DepartmentName, Price, StockQuantity FROM Products WHERE ?'
+        connection.query(query, {food: answer.food}, function(err, res) {
+            for (var i = 0; i < answer.length; i++) {
+                console.log("ID:" + res[i].id + " || ProductName: " + res[i].ProductName + " || DepartmentName:" + res[i].DepartmentName + " || Price:" + res[i].Price + " || StockQuantity:" + res[i].StockQuanity);
+            }
             runSearch();
         })
     })
 };
-*/
-
-recheck commmented lines of code
